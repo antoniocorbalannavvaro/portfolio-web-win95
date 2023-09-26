0220b3689b45e1bdcd3193545cded6a8acaf9f48
Antonio Corbalán Navarro <antoniocn1996@gmail.com>
2023-09-26 16:47:30 +0200

    Update Experiencia.jsx

--- src/components/Content/Experiencia/Experiencia.jsx
+++ src/components/Content/Experiencia/Experiencia.jsx
@@ -16,7 +16,7 @@ const Experiencia = () => {
           <div className={style.content}>
             <h2 className={style.descriptionTitle}>Decripción:</h2>
             <div className={style.textContent}>
-              <h5>Convolutional Port es una start-up con sede en Murcia.</h5>
+              <h5>Convolutional Port es un proyecto personal que realicé junto a un compañero.</h5>
               <h5>El proyecto que desarrollamos soluciona uno de los problemas más recurrentes</h5>
               <h5>en los puertos de recreo españoles; registrar las entradas y salidas</h5>
               <h5>de los barcos en los puertos y la monitorización en tiempo real de</h5>
@@ -33,9 +33,6 @@ const Experiencia = () => {
               <h5>al amarre de las embarcaciones. De esta forma, cuando una embarcación entra</h5>
               <h5>en el puerto, salta una alarma con la matrícula y punto de amarre correspondiente</h5>
               <h5>para poder asistir correctamente al amarre y evitar posibles accidentes.</h5>
-              <br></br>
-              <h5>Este proyecto se realizó a petición de PANDORA GLOBAL SL, principal proveedora</h5>
-              <h5>de software de gestión portuaria en España.</h5>
             </div>
           </div>
         </section>
@@ -54,8 +51,8 @@ const Experiencia = () => {
               <h5>UNNITATION es una Annotation Tool orientada al etiquetado de emociones humanas</h5>
               <h5>en videos de reuniones online de negocios en las principales plataformas de meeting:</h5>
               <h5>Google Meet, Microsoft Team, Skype...</h5>
-              <h5>Este proyecto se realizó para UNIPHORE, empresa de Silicon Valley con sede en</h5>
-              <h5>Valencia.</h5>
+              <h5>Este proyecto se realizó pensando en las necesidades UNIPHORE,</h5>
+              <h5>empresa Valenciana.</h5>
               <br/>
               <h5>UNIPHORE comercializa un software capaz de valorar, mediante un modelo de</h5>
               <h5>inteligencia artificial, si una reunión de negocios ha sido satisfactoria o</h5>
